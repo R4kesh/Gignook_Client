878b7452c97926f1fed9a800bcf706a1d08f5adb
Rakes S <rakeshsrks2580@gmail.com>
2024-07-26 10:09:26 +0530

    env setup

--- src/lib/notification.tsx
+++ src/lib/notification.tsx
@@ -6,43 +6,11 @@ const socket = io(`${process.env.NEXT_PUBLIC_BASE_URL}`,{
   transports:['websocket']
 }); // Adjust the URL to match your server's address
 
-// useEffect(() => {
-//     socket.on('notification', (data) => {
-//       console.log(data); // Handle the notification data
-//       // Update your UI based on the notification type
-//     });
-  
-//     return () => {
-//       socket.off('notification');
-//     };
-//   }, []);
+
 
   export default socket;
 
 
 
 
-  
-// import { useEffect } from 'react';
-// import io from 'socket.io-client';
-
-// const notification = () => {
-
-  
-// const socket = io('http://localhost:5001'); // Adjust the URL to match your server's address
-
-// useEffect(() => {
-//     socket.on('notification', (data) => {
-//       console.log(data); // Handle the notification data
-//       // Update your UI based on the notification type
-//     });
-  
-//     return () => {
-//       socket.off('notification');
-//     };
-//   }, []);
-
-  
-// }
-
-// export default notification
+ 
